f053afb1e6d45056c9b6712bc968c0736c05d735
burYE <snapbackluffy@gmail.com>
2019-09-08 23:34:24 -0400

    Cube and Capsule Movement codes
    + the scene

---
+++ CapsuleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+           transform.position = new Vector3(0, 7 * Mathf.Cos(Time.time), 20 * Mathf.Sin(Time.time));
+
+        
+    }
+}
